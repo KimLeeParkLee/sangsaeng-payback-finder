42522422aa787937d79e1bb995d309bc4a2cd7c8
kn9012 <kn9012@naver.com>
2025-10-08 23:51:46 +0900

    Initial commit

---
+++ src/main/java/com/sangsaengpaybackfinder/sangsaengpaybackfinder/SangsaengpaybackfinderApplication.java
@@ -0,0 +1,13 @@
+package com.sangsaengpaybackfinder.sangsaengpaybackfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangsaengpaybackfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangsaengpaybackfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sangsaengpaybackfinder/sangsaengpaybackfinder/SangsaengpaybackfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangsaengpaybackfinder.sangsaengpaybackfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangsaengpaybackfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
